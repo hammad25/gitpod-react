cedde39b11e6408720dfcade4bb30dc178cd766f
Hammad <hammad2091504@hotmail.com>
2022-11-27 17:43:57 +0000

    Add compnents, make arrow function, export default, import

---
+++ my-app/src/components/FunctionalGreetings.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+const FunctionalGreetings = () => {
+    return ( <
+        h1 > Hello 1 from React < /h1>
+    )
+}
+
+
+// function FunctionalGreetings(){
+//     return <h1>Hello from React</h1>
+// }
+
+
+export default FunctionalGreetings
---
+++ my-app/src/components/FunctionalGreetingsWithProps.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+// 1. make components folder
+// 2. js file 
+// 3. create arrow function, export, import in App
+
+const FunctionalGreetingsWithProps = () => < h1 > Hello < /h1>;
+
+
+export default FunctionalGreetingsWithProps
