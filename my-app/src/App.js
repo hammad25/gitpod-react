c50014cb9e14e42c6e658c6caa4091689c8304d5
Hammad <hammad2091504@hotmail.com>
2022-11-27 19:11:42 +0000

    Add props to components, retrive props in fucntion as parameter and rendering using {}

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,14 +4,11 @@ import FunctionalGreetings from "./components/FunctionalGreetings";
 import FunctionalGreetingsWithProps from "./components/FunctionalGreetingsWithProps";
 
 function App() {
-  return ( <
-    div className = "App" >
-    <
-    FunctionalGreetings / >
-    <
-    FunctionalGreetingsWithProps / >
-    <
-    /div>
+  return (
+    <div className="App">
+      <FunctionalGreetings/>
+      <FunctionalGreetingsWithProps greeting="Nice to meet you!" name="Mike" age="32"/>
+    </div>
   );
 }
 
--- my-app/src/components/FunctionalGreetingsWithProps.js
+++ my-app/src/components/FunctionalGreetingsWithProps.js
@@ -3,8 +3,12 @@ import React from "react";
 // 1. make components folder
 // 2. js file 
 // 3. create arrow function, export, import in App
+// 4. adding props to component, retriving props and rendering using {}
 
-const FunctionalGreetingsWithProps = () => < h1 > Hello < /h1>;
+const FunctionalGreetingsWithProps = (props) => {
+    console.log(props)
+    return <h1> Hello, {props.name}! I see you're {props.age} years old; {props.greeting} </h1>;
+}
 
 
 export default FunctionalGreetingsWithProps
