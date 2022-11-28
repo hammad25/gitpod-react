bf0ecd387744acf4e1efdd7b7b2bdb9b7c6f9563
Hammad <hammad2091504@hotmail.com>
2022-11-28 15:24:15 +0000

    add onClick, then add event handler which is handleClick and inside it use this.setState({})

--- my-app/src/components/StatefulGreeting.js
+++ my-app/src/components/StatefulGreeting.js
@@ -9,11 +9,24 @@ class StatefulGreeting extends React.Component {
             buttonText: "Exit",
         };
     }
+
+    handleClick(){
+        // use setState to update 
+        this.setState({
+            introduction: "Goodbye!",
+            buttonText: "Enter",
+        });
+        console.log(this.state.introduction);
+    }
+
     render(){
         return (
             <div>
                 <h1>{this.state.introduction} {this.props.name} {this.props.greeting}</h1>
-                <button>{this.state.buttonText}</button>
+                {/* updating the state with onClick(), then use an event handler to listen for the click
+                and use setState() to update the state */}
+                {/* onClick a js hence in { } and ()=> */}
+                <button onClick={()=> this.handleClick()}>{this.state.buttonText}</button>
             </div>
         ) 
     }
