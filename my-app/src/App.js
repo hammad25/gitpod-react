e4b4d2d902a773c5cfa78dac7c3a2d7eb5707417
Hammad <hammad2091504@hotmail.com>
2022-11-28 14:20:17 +0000

    Add state, use contructor, super, and use this.state as object for inital state

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import FunctionalGreetings from "./components/FunctionalGreetings";
 import FunctionalGreetingsWithProps from "./components/FunctionalGreetingsWithProps";
+import StatefulGreeting from './components/StatefulGreeting';
 
 function App() {
   return (
     <div className="App">
       <FunctionalGreetings/>
       <FunctionalGreetingsWithProps greeting="Nice to meet you!" name="Mike" age="32"/>
+      <StatefulGreeting greeting="Im a stateful class component" name="Hammad"/>
     </div>
   );
 }
